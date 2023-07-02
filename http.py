eeabb937cea51094a702dfaed77311fb0c0f0fb0
cyber-bytezz <130319315+cyber-bytezz@users.noreply.github.com>
2023-07-02 13:54:30 +0530

    Update http.py

--- http.py
+++ http.py
@@ -1,31 +1,34 @@
-#HTTP FUZZING
+ from sulley import *
 
-#ITS AND FUZZING LIBIRAIES
+# Get user input for target IP address and port
+target_ip = input("Enter the target IP address: ")
+target_port = int(input("Enter the target port: "))
 
-#REQUEST USED FOR FUZZING
+# Create a new session
+sess = sessions.session()
 
-from sully import *
+# Define the target
+target = sessions.target(target_ip, target_port)
 
-#REQUEST GETS FROM (GET,BODY,POST) HTTP 1.1
+# Define the protocol
+sess.add_target(target)
+sess.connect(s_get("HTTP BASIC"))
 
 s_initialize("HTTP BASIC")
 
 s_group("verbs", values=["GET","HEAD","POST","TRACE"])
 
-if s_block_start ("body","group","trace")
+if s_block_start ("body","group","trace"):
+    s_delim("")
+    s_delim("/")
+    s_string("index.html")
+    s_delim(" ")
+    s_string("HTTP")
+    s_delim("/")
+    s_string("1")
+    s_delim(".")
+    s_string("1")
 
-#HERE ARE THE REQUEST
-
-s_delim("")
-s_delim("/")
-s_string("index.html")
-s_delim(" ")
-s_string("HTTP")
-s_delim("/")
-s_string("1")
-s_delim(".")
-s_string("1")
-
-s_static("/r/n/r/n")
+s_static("\r\n\r\n")
 
 s_block_end("body")
